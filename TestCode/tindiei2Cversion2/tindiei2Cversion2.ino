e87e2a2b8f46aacd505658f224aeaef8307dbe80
ananaw <61036678+ananaw@users.noreply.github.com>
2021-02-06 18:51:34 -0800

    Add files via upload

---
+++ TestCode/tindiei2Cversion2/tindiei2Cversion2.ino
@@ -0,0 +1,27 @@
+// To use this code, I installed the I2CSoilMoistureSensor Library provided by Ingo Fischer in Arduino.
+#include <I2CSoilMoistureSensor.h>
+#include <Wire.h>
+
+I2CSoilMoistureSensor sensor;
+
+void setup() {
+  Wire.begin();
+  Serial.begin(115200);
+
+  sensor.begin(); // reset sensor
+  delay(1000); // give some time to boot up
+  Serial.print("I2C Soil Moisture Sensor Address: ");
+  Serial.println(sensor.getAddress(),HEX);
+  Serial.print("Sensor Firmware version: ");
+  Serial.println(sensor.getVersion(),HEX);
+  Serial.println();
+}
+
+void loop() {
+  while (sensor.isBusy()) delay(50); // available since FW 2.3
+  Serial.print("Soil Moisture Capacitance: ");
+  Serial.println(sensor.getCapacitance()); //read capacitance register
+  Serial.print(", Temperature: ");
+  Serial.println(sensor.getTemperature()/(float)10); //temperature register
+  sensor.sleep(); // available since FW 2.3
+}
