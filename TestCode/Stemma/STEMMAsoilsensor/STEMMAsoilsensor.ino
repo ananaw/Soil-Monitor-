c2fb2a8253d4cc3797d4476f810d80f1366b84fa
ananaw <61036678+ananaw@users.noreply.github.com>
2021-03-13 19:19:39 -0800

    Update STEMMAsoilsensor.ino

--- TestCode/Stemma/STEMMAsoilsensor/STEMMAsoilsensor.ino
+++ TestCode/Stemma/STEMMAsoilsensor/STEMMAsoilsensor.ino
@@ -12,11 +12,18 @@
  * 10K resistor:
  * ends to +5V and ground
  * wiper to LCD VO pin (pin 3)
+ * 
+ * STEMMA soil sensor
+ * red wire to 3-5V,
+ * black wire to GND
+ * SCL Green wire to A5
+ * SDA white wire to A4
  */
 
 // include the library code:
 #include <LiquidCrystal.h>
 #include "Adafruit_seesaw.h"
+#include <LowPower.h>       // Library written by RocketScream 
 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
@@ -28,7 +35,7 @@ int intervals = (WaterVal - AirVal)/3;
 int soilMoistureVal = 0;
   
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
 
   Serial.println("seesaw Soil Sensor example!");
   
@@ -45,6 +52,9 @@ void setup() {
   // lcd.setCursor(2,1); print 3 col from left and 2nd row. (col,row) from 0-15 and 0-1.
   // Print a message to the LCD.
   lcd.print("Hello, World!");
+
+  // set internal LED as output, used for indicating whether arduino has woken up
+  pinMode(LED_BUILTIN, OUTPUT); 
 }
 
 void loop() {
@@ -53,6 +63,9 @@ void loop() {
  
   uint16_t capread = ss.touchRead(0);
 
+  // turn off LED
+  digitalWrite(LED_BUILTIN, LOW);
+  
   Serial.print("Temperature: "); Serial.print(tempC); Serial.print("*C; "); Serial.print(tempF); Serial.println(" degrees F");
   Serial.print("Capacitive: "); Serial.println(capread);
 
@@ -62,25 +75,19 @@ void loop() {
   if(percentage >= 75) { 
     Serial.println("Wet"); } 
   else if(35 <= (percentage) && (percentage) < 75)
-{
+  {
   Serial.println("Moist");
-}
-else if(percentage < 35)
-{
+  }
+  else if(percentage < 35)
+  {
   Serial.println("Dry");
-}
+  }
   
   delay(1000);
+  
+  digitalWrite(LED_BUILTIN, HIGH);  // Turn on LED to indicate Arduino going to sleep.
+  delay(500);
+  
+  // go to deep sleep for 8s, allows power optimization with slowest wake up time.
+  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF); 
 }
-
-
-//if(soilMoistureVal >= (WaterVal - intervals)) { 
-//    Serial.println("Very Wet"); } 
-//  else if(soilMoistureVal <= (WaterVal - intervals) && soilMoistureVal >= (AirVal + intervals))
-//{
-//  Serial.println("Wet");
-//}
-//else if(soilMoistureVal <=  (AirVal + intervals))
-//{
-//  Serial.println("Dry");
-//}
